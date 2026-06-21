9abe8c2d9bc8a4343de43d17dbf6db69314998ca
Angus Hardy-Francis <angushf@gmail.com>
2026-06-21 16:24:01 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Variable window
+        # Condition - if R is <= L (smallest seen so far), L = R
+        # Update global variable with prices[r] - prices[l]
+
+        l = 0
+
+        result = 0
+
+        for r, price in enumerate(prices):
+            # ADD
+            result = max(result, prices[r] - prices[l])
+
+            # Shrink
+            if prices[r] <= prices[l]:
+                l = r
+
+        return result
