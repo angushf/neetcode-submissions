733d6b1a97784cb91721ac0fa60231b9ec6b783a
Angus Hardy-Francis <angushf@gmail.com>
2026-06-23 10:47:13 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # Bisect_left type of BS
+        # Once we identify which of the two sorted runs our min is in
+        # we can use bisect left to find min element
+
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if nums[mid] < nums[hi]:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return nums[lo]
