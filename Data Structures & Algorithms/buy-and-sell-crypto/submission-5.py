105a8a387422498d4bd376e09984601a28af08b4
Angus Hardy-Francis <angushf@gmail.com>
2026-07-05 12:47:07 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Variable window
+        
+        profit = float("-inf")
+        l = 0
+        
+        for r, price in enumerate(prices):
+            profit = max(profit, price - prices[l])
+
+            if price < prices[l]:
+                l = r
+
+        return profit
