d064501a22c1cc3bc2df05c70d53bb98d05c3fb1
Angus Hardy-Francis <angushf@gmail.com>
2026-08-08 18:12:46 -0700

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        # iterate through 2D grid and start a DFS search when an element == "1"
+        # increment result by 1 after we finish each DFS
+        # turn every 1 into "0" during dfs
+        # return result after iterating through grid
+
+        result = 0
+
+        def dfs(row, col, visited):
+            if row < 0 or row > len(grid)-1:
+                return
+
+            if col < 0 or col > len(grid[0])-1:
+                return
+
+            if grid[row][col] == "0":
+                return
+
+            # visited.append((row, col))
+            grid[row][col] = "0"
+
+            dfs(row-1, col, visited)
+            dfs(row+1, col, visited)
+            dfs(row, col+1, visited)
+            dfs(row, col-1, visited)
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    dfs(r, c, [])
+                    result += 1
+
+        return result
